40ee625a1472470fded8987d416a906f65b89dbb
chanhpark <chanhyun.park1211@gmail.com>
2014-10-24 12:40:39 -0400

    Fizz Buzz using the each iterator

--- Fizz_Buzz.rb
+++ Fizz_Buzz.rb
@@ -1,3 +1,4 @@
+#v1
 1.upto(100) do |x|
   if (x % 3 == 0 and x % 5 == 0)
     puts 'FizzBuzz'
@@ -9,3 +10,20 @@
     puts x
   end
 end
+
+#v2
+def fizz(n)
+  (1..n).each do |i|
+    if i % 3 == 0 or i % 5 == 0
+      puts 'FizzBuzz'
+    elsif i % 3 == 0
+      puts 'Fizz'
+    elsif i % 5 == 0
+      puts 'Buzz'
+    else
+      puts i
+    end
+  end
+end
+
+fizz(100)
